3e4aa4e067aaf2f77e2317d6a6693560403041bf
Tim Engelmann <tim.engelmann@mailbox.org>
2023-03-24 16:55:57 +0100

    Added padding

--- src/models/utils/dataset.py
+++ src/models/utils/dataset.py
@@ -5,7 +5,7 @@ import rasterio
 from torch.utils.data import Dataset
 
 # function to load biomass data
-def load_processed_biomass_data(year, shape=None, resolution=250):
+def load_processed_biomass_data(year, shape=None, resolution=250, delta=None):
     path_bio = f"data/processed/biomass/amazonia/{resolution}m/" + f"biomass_{year}.tif"
     with rasterio.open(path_bio) as src:
         out_meta = src.meta
@@ -20,10 +20,14 @@ def load_processed_biomass_data(year, shape=None, resolution=250):
         else:
             bio_data = src.read(1)
     bio_data = torch.from_numpy(bio_data)
+    if delta is not None:
+        diff_x = int((bio_data.shape[1]%delta)/2 + delta)
+        diff_y = int((bio_data.shape[0]%delta)/2 + delta)
+        bio_data = torch.nn.functional.pad(bio_data, (diff_y, diff_y, diff_x, diff_x), value=255)
     return bio_data, out_meta
 
 # function to load transition data
-def load_processed_transition_data(year_start, year_end, shape=None, resolution=250):
+def load_processed_transition_data(year_start, year_end, shape=None, resolution=250, delta=None):
     path_bio = f"data/processed/biomass/amazonia/{resolution}m/" 
     path_bio = path_bio + f"transition_{year_start}_{year_end}.tif"
     with rasterio.open(path_bio) as src:
@@ -39,11 +43,15 @@ def load_processed_transition_data(year_start, year_end, shape=None, resolution=
         else:
             transition_data = src.read(1).astype(bool)
     transition_data = torch.from_numpy(transition_data)
+    if delta is not None:
+        diff_x = int((transition_data.shape[1]%delta)/2 + delta)
+        diff_y = int((transition_data.shape[0]%delta)/2 + delta)
+        transition_data = torch.nn.functional.pad(transition_data, (diff_y, diff_y, diff_x, diff_x), value=False)
     return transition_data, out_meta
 
 
 class DeforestationDataset(Dataset):
-    def __init__(self, dataset, resolution=250, input_px=400, output_px=40, max_elements=None):
+    def __init__(self, dataset, resolution=250, input_px=400, output_px=40, delta=360, max_elements=None):
         """
         Args:
             dataset: "train", "val", "test"
@@ -55,6 +63,7 @@ class DeforestationDataset(Dataset):
         self.resolution = resolution
         self.input_px = input_px
         self.output_px = output_px
+        self.delta = delta
         
         # dataformat: [x_point, y_point, x_cluster, y_cluster]
         data = torch.load(f'data/processed/{dataset}_data.pt')
@@ -64,15 +73,15 @@ class DeforestationDataset(Dataset):
                 self.data = data[:max_elements]
 
         feature_data = []
-        bio_data, _ = load_processed_biomass_data(self.year, resolution=self.resolution)
+        bio_data, _ = load_processed_biomass_data(self.year, resolution=self.resolution, delta=self.delta)
         feature_data.append(bio_data)
     
         for past_horizon in self.past_horizons:
-            transition_data, _ = load_processed_transition_data(self.year - past_horizon, self.year, resolution=self.resolution)
+            transition_data, _ = load_processed_transition_data(self.year - past_horizon, self.year, resolution=self.resolution, delta=self.delta)
             feature_data.append(transition_data)
         self.feature_data = torch.stack(feature_data)
 
-        target_data, _ = load_processed_transition_data(self.year, self.year+self.future_horizon, resolution=self.resolution)
+        target_data, _ = load_processed_transition_data(self.year, self.year+self.future_horizon, resolution=self.resolution, delta=self.delta)
         self.target_data = target_data
         
     def __len__(self):
