c1e49bdcad6b3cac25c52f2554c716ff6e62ef70
Tim Engelmann <tim.engelmann@mailbox.org>
2023-04-22 19:40:57 +0200

    Runs on Euler

--- submit.sh
+++ submit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --mem-per-cpu=64G
-#SBATCH --tmp=300G  
+#SBATCH --cpus-per-task=5
+#SBATCH --mem-per-cpu=8G
 #SBATCH --gpus=1
 #SBATCH --gres=gpumem:8G
-#SBATCH --time=4:00:00
+#SBATCH --time=8:00:00
 
 python3 src/models/train_model.py $1
  
